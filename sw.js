1e5f3fd0d201089c6c77a6a0788d9382b8bc02e6
PeteStacy <82793220+PeteStacy@users.noreply.github.com>
2026-08-11 18:03:20 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='poly-dose-v2';const A=['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
