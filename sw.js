316223f5df2acbc238cecbd36051d1f6d1e4e563
PeteStacy <82793220+PeteStacy@users.noreply.github.com>
2026-08-12 19:12:21 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='sludgeulator-v4';const A=['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const C='sludgeulator-v5';const A=['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
